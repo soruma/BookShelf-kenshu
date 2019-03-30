e787b7ea08908617bb7c57309b7f3a0b6f585e38
soruma <soruma.net@gmail.com>
2019-03-30 13:56:23 +0900

    add fixtures book and person

---
+++ db/fixtures/01_persons.rb
@@ -0,0 +1,6 @@
+Person.seed(:id,
+  { id: 1, name: 'Example Person 1' },
+  { id: 2, name: 'Example Person 2' },
+  { id: 3, name: 'Example Person 3' },
+  { id: 4, name: 'Example Person 4' },
+)
---
+++ db/fixtures/02_books.rb
@@ -0,0 +1,6 @@
+Book.seed(:id,
+  { id: 1, title: '書籍 1', author_id: 1 },
+  { id: 2, title: '書籍 2', author_id: 2 },
+  { id: 3, title: '書籍 3', author_id: 2 },
+  { id: 4, title: '書籍 4', author_id: 3 },
+)
